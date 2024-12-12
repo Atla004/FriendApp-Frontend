485ddd25b5d23852381663e88d7090f13a6bf6fc
Atla004 <andrese.g.v.13579@gmail.com>
2024-12-12 15:31:54 -0400

    feat: update socket connection with enhanced options and error handling

--- utils/socket.ts
+++ utils/socket.ts
@@ -1,8 +1,23 @@
-import io, { Socket  } from 'socket.io-client';
+import io from 'socket.io-client';
 
 const SocketServerUrl = process.env.EXPO_PUBLIC_WS_URL as string
 
-export const socket = io(SocketServerUrl);
+
+
+export const socket = io( "wss://txbq7qkk-6900.use2.devtunnels.ms", {
+  transports: ['websocket'], 
+  reconnectionAttempts: 2, 
+  reconnectionDelay: 10000,
+});
+
+socket.on('connect', () => {
+  console.log('Connected to WebSocket server');
+  socket.send('connected');
+});
+
+socket.on('connect_error', (error:any) => {
+  console.log('Connection failed:', error);
+});
 
 export const joinChat = (chat_id: string) => {
     socket.send('join', chat_id)
